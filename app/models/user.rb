25cba1b3015f254b79a95ce2e4ce777ff47d7533
PandaGrom <arsenij.grom@mail.ru>
2022-01-20 14:11:00 +0300

    create folder validators and move phone validation from model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
-class PhoneNumberValidator < ActiveModel::Validator
-  def validate(record)
-    return if Phonelib.valid? record.phone_number
-
-    record.errors.add :phone_number, 'Invalid telephone number'
-  end
-end
-
 class User < ApplicationRecord
   include UserStatus
 
