13038283a453f7f24d8f3c41a6626dcfc34406e8
alhimick-prog <alhimick96@yahoo.com>
2022-01-20 13:59:23 +0300

    Revert "Added indexes"
    This reverts commit 9f90284ee136060639301258266401a3787eb1ca.

--- db/migrate/20220119173605_add_indexes_to_columns_in_tables_patients_and_allergies.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexesToColumnsInTablesPatientsAndAllergies < ActiveRecord::Migration[6.1]
-  def change
-    add_index(:patients, [:email, :passport_id], unique: true)
-    add_index(:allergies, :name, unique: true)
-  end
-end
