4bd35115ceb3d2443a85511930b2bffad9c26128
alhimick-prog <alhimick96@yahoo.com>
2022-01-26 22:21:06 +0300

    Read for patient(s), entity for allergy, start create for patients

--- app/api/entities/patient.rb
+++ app/api/entities/patient.rb
@@ -1,16 +1,16 @@
-
 module Entities
   class Patient < Grape::Entity
     expose :id
     expose :address
-    expose :allergies_additional
     expose :date_of_birth
     expose :email
     expose :full_name
     expose :notes
     expose :telephone_number
     expose :passport_id
-    #expose :allergies
-    #expose :picture
+    expose :allergies do
+      expose :allergies, using: API::Entities::Allergy
+      expose :allergies_additional
+    end
   end
 end
---
+++ app/api/v1/patients.rb
@@ -0,0 +1,40 @@
+module V1
+  class Patients < API
+    resources :patients do
+
+      desc 'Return all patients'
+      get do
+        @patients = Patient.all
+        present @patients, with: Entities::Patient
+      end
+
+      desc 'Return specific patient'
+      route_param :id, type: Integer do
+        get do
+          @patient = Patient.find(params[:id])
+          present @patient, with: Entities::Patient
+        end
+      end
+
+      desc 'Create new patient'
+      post do
+        params[:address]
+        params[:date_of_birth]
+        params[:email]
+        params[:full_name]
+        params[:notes]
+        params[:telephone_number]
+        params[:passport_id]
+        params[:allergies_additional]
+
+        patient = Patient.new(params)
+        if patient.save
+          redirect to patient
+        else
+          error!({ error_code: 404, error_message: 'Invalid Email or Password.' }, 401)
+          return
+        end
+      end
+    end
+  end
+end
